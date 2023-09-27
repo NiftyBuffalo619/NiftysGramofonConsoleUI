8a7f020960e23200886239d1338f0f60fcd5e286
NiftyBuffalo619 <polaclukes@outlook.cz>
2023-09-27 23:34:55 +0200

    Fixed searching

--- main.py
+++ main.py
@@ -1,69 +1,64 @@
-from textual.app import App
-from textual.widgets import Footer, Header, Button, Static
+from __future__ import annotations
+
+from functools import partial
+from pathlib import Path
+
+from rich.syntax import Syntax
+
+from textual.app import App, ComposeResult
 from textual.command import Hit, Hits, Provider
 from textual.containers import VerticalScroll
-import winsound
-from functools import partial
+from textual.widgets import Static, Footer, Header
 
-class NowPlaying(Static):
-    """Some Text"""
-class AnotherCommand(Provider):
-    async def search(self, query:str) -> Hits:
-        matcher = self.matcher(query)
-        
-        command = "play"
-        score = matcher.match(command)
-        yield Hit(
-                score,
-                matcher.highlight(command),  
-                partial(),
-                help="Plays a test sound",
-            )
-class Commands(Provider):
-    list = ["FNAF"]
-    
-    """Plays a sound"""
-    
-    def read_url(self) -> []:
-        return list
-    async def startup(self) -> None:
-        await self.playsound()
+
+class PlaySong(Provider):
     async def search(self, query: str) -> Hits:
-        """Search"""
         matcher = self.matcher(query)
         app = self.app
-        assert isinstance(app , NiftyhoGramofon)
-        for url in list:
-            command = f"connect {str(url)}"
-            score = matcher.match(command)
-            if score > 0:
-                yield Hit(
-                    score,
-                    matcher.highlight(command),  
-                    partial(),
-                    help="Connect to FNAF",
-                )
-    def playsound():
-        return windsound.PlaySound("SystemQuestion", winsound.SND_ALIAS)
-        """"""
+        assert isinstance(app, NiftyhoGramofonUI)
+        command = f"play"
+        score = matcher.match(command)
+        if score > 0:
+            yield Hit(
+                score,
+                matcher.highlight(command),
+                partial(app.playsong),
+                help="Plays a song by the id provided",
+            )
+class Playsound(Provider):
+    async def search(self, query: str) -> Hits:  
+        matcher = self.matcher(query)  
 
-class NiftyhoGramofon(App):
-    
-    CSS_PATH = "style.tcss"
+        app = self.app
+        assert isinstance(app, NiftyhoGramofonUI)
+        command = f"playtest"
+        score = matcher.match(command)  
+        if score > 0:
+            yield Hit(
+                score,
+                matcher.highlight(command),  
+                partial(app.do_something),
+                help="Plays a test sound",
+            )
+
+class NiftyhoGramofonUI(App):
     BINDINGS = [
         ("d", "change_theme", "Change Theme"),
         ("q", "quit", "Quit"),
     ]
-    COMMANDS = App.COMMANDS | {Commands, AnotherCommand}
-    def compose(self):
-        yield Header(show_clock=True)
+    COMMANDS = App.COMMANDS | {Playsound} | {PlaySong}
+    def compose(self) -> ComposeResult:
+        yield Header()
         yield Footer()
-        yield NowPlaying("Now Playing something", id="now_playing")
-    # IMPORTANT there has to be an action_ in it because it is an action method and it's associated with change_theme
+        with VerticalScroll():
+            yield Static(id="code", expand=True)
+    def do_something(self) -> None:
+        self.bell()
     def action_change_theme(self):
         self.dark = not self.dark
-    def action_playsound(self):
-        self.bell()
+    def playsong(self) -> None:
+        """"""
 
 if __name__ == "__main__":
-    NiftyhoGramofon().run()
+    app = NiftyhoGramofonUI()
+    app.run()
